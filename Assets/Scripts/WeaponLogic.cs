76ed28cfda6e787c84b3afca746b6d77e56cc5aa
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-07-18 19:02:51 +0300

    Initial commit

---
+++ Assets/Scripts/WeaponLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLogic : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //StartCoroutine(WeaponFlying());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.up,ForceMode2D.Force);
+    }
+    IEnumerator WeaponFlying()
+    {
+        while (true)
+        {
+            rb.AddForce(transform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    public GameObject weapon;
+    void Start()
+    {
+        StartCoroutine(Attacking());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Attacking()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(weapon, transform.forward, Quaternion.identity);
+            }
+            yield return null;
+        }
+    }
+}
